900b63a13031973a6e4c0f24ea7612719f52cf26
captainvinager <captainvinager@gmail.com>
2021-01-31 21:02:40 +0100

    add anna sprite

--- src/app/gameScene.ts
+++ src/app/gameScene.ts
@@ -10,6 +10,7 @@ export class GameScene extends Phaser.Scene{
     //ball : Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
     //ball : Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     anna : Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    dude : Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     pushedKey: Phaser.Types.Input.Keyboard.CursorKeys;
     actionObject : DropItem;
     map : Phaser.Tilemaps.Tilemap;
@@ -30,6 +31,10 @@ export class GameScene extends Phaser.Scene{
         this.load.spritesheet('player','../assets/images/dude.png',{
             frameWidth: 32,
             frameHeight: 48
+        })
+        this.load.spritesheet('anna','../assets/images/anna.png',{
+            frameWidth: 64,
+            frameHeight: 64
         })
         this.load.image('ball','../assets/images/ball.png');
     }
@@ -60,14 +65,14 @@ export class GameScene extends Phaser.Scene{
             this.setStopAction();
         });*/
         
-        //animaciones del Player
+        //animaciones del dude
 
-        this.anna = this.physics.add.sprite(50,165,"player")
+        this.dude = this.physics.add.sprite(50,165,"player")
 
-        this.physics.add.collider(this.anna,wallLayer,()=>{
+        this.physics.add.collider(this.dude,wallLayer,()=>{
             this.setStopAction();
         });
-        this.anna.anims.play("spin")
+    
         this.anims.create({
                 key: "right",
                 frameRate: 10,
@@ -86,7 +91,44 @@ export class GameScene extends Phaser.Scene{
                 frames : this.anims.generateFrameNumbers("player",{ start: 0, end: 3}),
                 repeat : -1
             })
-       
+       //animaciones de anna
+
+       this.anna = this.physics.add.sprite(100,165,"anna")
+
+       this.physics.add.collider(this.anna,wallLayer,()=>{
+           this.setStopAction();
+       });
+   
+       this.anims.create({
+               key: "rightAnna",
+               frameRate: 10,
+               frames : this.anims.generateFrameNumbers("anna",{ start: 27, end: 35}),
+               repeat : -1
+           })
+       this.anims.create({
+               key: "stopAnna",
+               frameRate: 20,
+               frames : [{ key: "anna", frame :18}]
+              
+           })
+           this.anims.create({
+               key: "leftAnna",
+               frameRate: 10,
+               frames : this.anims.generateFrameNumbers("anna",{ start: 17, end: 9}),
+               repeat : -1
+           })
+           this.anims.create({
+            key: "up",
+            frameRate: 10,
+            frames : this.anims.generateFrameNumbers("anna",{ start: 0, end: 8}),
+            repeat : -1
+        })
+        this.anims.create({
+            key: "down",
+            frameRate: 10,
+            frames : this.anims.generateFrameNumbers("anna",{ start: 18, end: 26}),
+            repeat : -1
+        })
         //recoge las pulsaciones de teclado
         this.pushedKey = this.input.keyboard.createCursorKeys();
 
@@ -97,7 +139,7 @@ export class GameScene extends Phaser.Scene{
     update() {
         //this.ball.setGravity(0);
         //this.ball.body.setVelocity(0,0);
-        this.anna.body.setVelocity(0,0);
+        this.dude.body.setVelocity(0,0);
       
         if(this.pushedKey.left.isDown){
             //this.ball.setGravity(0.1)
@@ -146,30 +188,39 @@ export class GameScene extends Phaser.Scene{
         }
         
         if(this.actionObject.label == "stop"){
-            this.anna.anims.play("stop",true)
+            this.dude.anims.play("stop",true)
+            this.anna.anims.play("stopAnna",true)
         }
     }
 
     private moveLeft(){
         //this.ball.setVelocity(this.velocity * -1,0);
+        this.dude.setVelocity(this.velocity * -1,0);
+        this.dude.anims.play("left",true)
         this.anna.setVelocity(this.velocity * -1,0);
-        this.anna.anims.play("left",true)
+        this.anna.play("leftAnna",true)
         //if(this.moveTime == 0) this.moveTime=10;
     }
     private moveRight(){
         //this.ball.setVelocity(this.velocity,0);
+        this.dude.setVelocity(this.velocity,0);
+        this.dude.anims.play("right",true)
         this.anna.setVelocity(this.velocity,0);
-        this.anna.anims.play("right",true)
+        this.anna.anims.play("rightAnna",true)
         //if(this.moveTime == 0) this.moveTime=10;
     }
     private moveUp(){
         //this.ball.setVelocity(0,this.velocity * -1);
         //if(this.moveTime == 0) this.moveTime=10;
+        this.anna.setVelocity(0,this.velocity *-1)
+        this.anna.play("up",true)
     }
 
     private moveDown(){
         //this.ball.setVelocity(0,this.velocity);
         //if(this.moveTime == 0) this.moveTime=10;
+        this.anna.setVelocity(0,this.velocity);
+        this.anna.play("down",true)
     }
 
     private setStopAction(){
